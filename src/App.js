662a9cc1f60983f80cc7f18cd7ffba6adf1d7e84
Francisco Possamai <franpossa1@gmail.com>
2022-06-23 07:02:05 -0300

    deploy githubpages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Container from "@mui/material/Container";
 import Homepage from "./pages/Homepage";
 import ChampPage from "./pages/ChampPage";
